6cc58e85d8b80023650cde6ad2ed971916df95bf
Student <susanwanjiku480@gmail.com>
2018-01-29 13:55:47 +0300

    first commit

---
+++ yvonne_cpp.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+\.
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+int addition (int x,int y) {int z =x+y;
+    return z;}
+int main()
+{int a;
+int b;
+cout <<"Enter first number";
+cin >>a;
+cout<<"enter second number"
