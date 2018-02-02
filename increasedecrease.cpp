e967d38f929363123cda233557ef96b55009e11a
Student <yvonnejahendah@gmail.com>
2018-02-02 12:12:08 +0300

    Added incremental code

---
+++ increasedecrease.cpp
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{int x = 5;
+for (int i =0;i<5;i ++){
+    cout<<x--;
+    cout<<"\n";}
+    
+    cout<< "\n";
+    x=5;
+    for (int i=0;i<5;i++){
+        cout<<--x;
+        cout<<"\n";
+    }
+    
+    
+
+    return 0;
+}
+
+
+
+
