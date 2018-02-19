7531d05a685ddff6b0e40c585f7db557ca44eddb
Student <yvonnejahendah@gmail.com>
2018-02-19 16:03:32 +0300

    added new file

---
+++ yvonnejahendah.cpp
@@ -0,0 +1,44 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, HTML, CSS, JS.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    
+    int a;
+    int b;
+    
+    cout<<"Ask for 1st number";
+        cin>>a;
+    cout<<"Ask for 2nd number";
+         cin>>b;
+         
+        if (a>b){
+            cout<<"a is greater";}
+            
+            else if(a==b) 
+           
+           
+          { cout<<"a is equal to b";}           
+                
+            
+            else{cout<<"b is greater";
+            
+            
+            
+            
+        }
+
+
+    return 0;
+    }
+
+
